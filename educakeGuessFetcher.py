ca240015ae5afeee2cccc214728b9de2fad3fc54
krishnaLikesToCode <kbhatgtr@gmail.com>
2026-02-17 22:32:16 +0400

    automated token reterival, cleaned code

--- educakeGuessFetcher.py
+++ educakeGuessFetcher.py
@@ -1,38 +1,92 @@
-import requests #extrernal PIP library import
+from curl_cffi import requests
 
-#getting URL to get questionIDs from
-browserUrl=(
-""
-)
+loginSession=requests.Session(impersonate="chrome120")
 
+username="krishnab0009"
+password="bedwarrior10"
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:146.0) Gecko/20100101 Firefox/146.0",
+    "Content-Type": "application/json",
+    "Accept": "application/json, text/plain, */*",
+    "Accept-Language": "en-GB,en;q=0.9",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Origin": "https://my.educake.co.uk",
+    "Referer": "https://my.educake.co.uk/student-login",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-origin",
+    "Sec-CH-UA": '"Chromium";v="120", "Google Chrome";v="120"',
+    "Sec-CH-UA-Mobile": "?0",
+    "Sec-CH-UA-Platform": '"Linux"',
+    "Connection": "keep-alive",
+}
+
+loginPayload={"username" : username, "password" : password, "lastname" : "", "userType" : "student","allowEmailForUsername" : True}
+
+
+def getTokens(loginPayload,verbose=False):
+    global loginHeaders,loginSession
+    
+    frontEndLoginURL="https://my.educake.co.uk/student-login"
+    loginURL="https://my.educake.co.uk/login"
+    sessionTokenURL="https://my.educake.co.uk/session-token"
+
+    # Step 1- Send GET to front-end login page (just used to get XSRF-TOKEN instance, could be any page, but this one has no redirects)
+    frontEndLoginPageResponse=loginSession.get(frontEndLoginURL,headers=headers)#send GET request
+    if verbose:print(f"Front-end page request finished with code {frontEndLoginPageResponse.status_code}, starting api-based login page request...\n\n\n")
+
+    oXSRFTOKEN=(loginSession.cookies.get_dict())['XSRF-TOKEN']#get site cookies stored in current session, extract XSRF-TOKEN
+    headers["X-XSRF-TOKEN"]=oXSRFTOKEN#Add to login headers
+
+    # Step 2- Send POST to the real login API, by sending Educake the login details in loginPayload, and Educake will refresh XSRF-TOKEN
+    # giving access/authorization to get a JWT token from my.educake.co.uk/session-token
+    apiLoginPageResponse = loginSession.post(loginURL,headers=headers,json=loginPayload)# send POST request with payload/login info
+    if verbose:print(f"Finished API login page response with code {apiLoginPageResponse.status_code}\n\n\n")
+    
+    XSRF_TOKEN=(apiLoginPageResponse.cookies.get_dict())['XSRF-TOKEN']# Get new XSRF-TOKEN
+    headers['X-XSRF-TOKEN']= XSRF_TOKEN# Replace old XSRF-TOKEN with new, permanent one
+
+    # Step 3- Take new XSRF-TOKEN and permissions, send GET to Educake session-token generator to create session-token (aka. JWT-TOKEN)
+    sessionTokenPageResponse = loginSession.get(sessionTokenURL,headers=headers)# send GET request
+    if verbose:print(f"Session token URL finished with code {sessionTokenPageResponse.status_code}")
+
+    sessionToken=sessionTokenPageResponse.json();sessionToken=sessionToken['accessToken']# Get the sent .json file, extract JWT-TOKEN
+    JWT_TOKEN=f"Bearer {sessionToken}"# Add 'Bearer' tag
+
+    if verbose:print("TOKEN fetching is done! Moving onto answer fetching...\n\n\n")
+
+    return [XSRF_TOKEN,JWT_TOKEN]
+
+
+
+
+# getting URL to get questionIDs from
+browserUrl=("https://my.educake.co.uk/my-educake/quiz/185054455")
 splitURL=browserUrl.split("/");quizID=splitURL[-1]
 urlToGoTo=f"https://my.educake.co.uk/api/student/quiz/{quizID}"
 
-#Definied security/session headers
-AUTH=(                      # MUST BE UPDATED EVRRY 30MINS below 
-""
-)
 
-XSRFTOKEN=(                 #MUST BE UPDATED EVERY NEW SESSION below 
-""
-)
+# Defining security tokens for request headers
+tokens=getTokens(loginPayload)
 
-#creating headers dict
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36',
-    'X-XSRF-TOKEN': XSRFTOKEN,
-    'Authorization': AUTH,
-}
+XSRF_TOKEN=tokens[0]
 
-#gets URL reponse to request
-urlResponse=requests.get(urlToGoTo,headers=headers)
+JWT_TOKEN=tokens[1]
 
 
-print(urlResponse.status_code)#give code (200 means success, 403 means JWT is probably expired, 404 is invalid quiz)
-print(urlResponse.reason)#gives reason for failure
+# updating headers with JWT-TOKEN
+headers['Authorization']= JWT_TOKEN
+
+# gets URL reponse to request
+urlResponse=loginSession.get(urlToGoTo,headers=headers)
+
+
+print(f"Got question IDs with code{urlResponse.status_code}, reason {urlResponse.reason}")#give code (200 means success, 403 means JWT is probably expired, 404 is invalid quiz)
+
 
 #Records text of URL
-responseAsText=urlResponse.text[:3000]
+responseAsText=urlResponse.text
 
 
 #sets start and end of where to look (starts at 'questions', finishes at end of questionIDs list)
@@ -54,10 +108,9 @@ for i in range(len(questionIDs)):
 
     sendPrompt={"givenAnswer" : "-1"}
 
-    answerURLresponse=requests.post(answerURL,headers=headers,data=sendPrompt)
-    answerResponseAsText=answerURLresponse.text[:2000]
+    answerURLresponse=loginSession.post(answerURL,headers=headers,json=sendPrompt)
+    answerResponseAsText=answerURLresponse.text
 
-    #print(answerResponseAsText)
 
     nstart=answerResponseAsText.find("\"correctAnswers\":[")
 
@@ -65,6 +118,8 @@ for i in range(len(questionIDs)):
 
     correctAnswers=((answerResponseAsText[nstart:nend]).replace("\"correctAnswers\":[","")).replace("\"","").split(",")
     print(f"Question {i+1} answer: {correctAnswers}")
+
+    
 
     
 
